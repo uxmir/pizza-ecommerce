bc9bd2477ecf4b8ddb98f4a20521feeab1b05429
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-05-01 20:00:57 +0600

    auth is done

---
+++ src/module/auth.model.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+const authSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    lowercase: true,
+    trim: true,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 6,
+    select: false,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "ConfirmPassword is required"],
+    validate: {
+      validator:function (el) {
+        return el === this.password;
+      },
+      message: "Password's are not same",
+    },
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin", "deliver"],
+    default: "user",
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    select: false,
+  },
+  refreshToken: {
+    type: String,
+    select: false,
+  },
+  resetPasswordToken: {
+    type: String,
+    select: false,
+  },
+  resetPasswordExpires: {
+    type: Date,
+    select: false,
+  },
+});
+
+authSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword=undefined
+  next();
+});
+
+authSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword,this.password);
+};
+
+export default mongoose.model("auth", authSchema);
